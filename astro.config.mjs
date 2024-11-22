32c1b2b798a6d64679c207831ef2b99b6095ac12
qrol <ggglondyn@gmail.com>
2024-11-22 20:17:37 +0100

    Fix contentful-management import and types v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from "@astrojs/node";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: node({
-    mode: "standalone"
+  adapter: netlify({
+    edgeMiddleware: true
   })
 });
