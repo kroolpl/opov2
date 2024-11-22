62125cc9f70cc38abf7689db41744e8990d36d46
qrol <ggglondyn@gmail.com>
2024-11-22 20:23:26 +0100

    Fix build configuration for Netlify deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import netlify from "@astrojs/netlify/functions";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: netlify()
+  output: 'hybrid',
+  adapter: netlify({
+    dist: new URL('./dist/', import.meta.url)
+  })
 });
